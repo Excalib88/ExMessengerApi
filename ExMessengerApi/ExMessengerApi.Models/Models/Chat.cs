9479afb29661143054803b1b6c6f2a29e2ac8ddb
razumovskiy <kolosovp94@gmail.com>
2020-08-24 18:11:49 +0300

    models premilinary version

---
+++ ExMessengerApi/ExMessengerApi.Models/Models/Chat.cs
@@ -0,0 +1,11 @@
+﻿namespace ExMessengerApi.Models.Models
+{
+    public class Chat
+    {
+        public int ChatId { get; set; }
+        public string Title { get; set; }
+        
+        // navigational property
+        // enum of users
+    }
+}
