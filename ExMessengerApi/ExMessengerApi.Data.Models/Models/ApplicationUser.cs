9621a41ff3576f574ca7f5e0d23df099edd2d5b9
Damir Mansurov <rave9373@gmail.com>
2020-08-24 18:54:43 +0300

    поменял айди юзера

--- ExMessengerApi/ExMessengerApi.Data.Models/Models/ApplicationUser.cs
+++ ExMessengerApi/ExMessengerApi.Data.Models/Models/ApplicationUser.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ExMessengerApi.Data.Models.Models
 {
-    public class ApplicationUser: IdentityUser, IEntity
+    public class ApplicationUser: IdentityUser<Guid>, IEntity
     {
-        public Guid Id { get; set; }
+        public override Guid Id { get; set; }
         public string FullName { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
